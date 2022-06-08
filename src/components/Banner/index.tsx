23f453d9a0c86ab0b8f62ce5c58adc077c527444
Paula Nader <paulampaan@gmail.com>
2022-06-08 14:44:36 -0300

    Creating Banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,5 @@
+import BannerImg from '../../assets/space-motors-picture.png';
+
+const Banner = () => <img src={BannerImg} alt="Banner" />;
+
+export default Banner;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import Banner from '../../components/Banner';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import { Wrapper } from '../../components/Wrapper';
@@ -6,6 +7,7 @@ export const Home: React.FC = () => {
     return (
         <Wrapper>
             <Header />
+            <Banner />
             <Footer />
         </Wrapper>
     );
